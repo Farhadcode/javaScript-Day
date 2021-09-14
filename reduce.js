ff1fab90641610db89f29b61fab9f1b8ed1425bd
Farhadcode <farhad.ahmed.aust@gmail.com>
2021-09-14 15:44:10 +0600

    reduct addad

--- reduce.js
+++ reduce.js
@@ -2,6 +2,7 @@
 
 const numebrArray = [7, 8, 9, 10, 6,];
 //const sum = (newValue, peviousValue) => newValue + peviousValue;
+
 const result = numebrArray.reduce((peviousValue, currentValue) => (peviousValue + currentValue), 5);
 console.log(result);
 
