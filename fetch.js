149c9b9ad2174c7961086b239bdbe872deaac094
Farhadcode <farhad.ahmed.aust@gmail.com>
2021-09-14 16:21:33 +0600

    reduct addad

--- fetch.js
+++ fetch.js
@@ -34,3 +34,43 @@ fetch(url)
 fetch("https://jsonplaceholder.typicode.com/users")
     .then(res => res.json())
     .then(data => console.log(data));
+
+// keys ,values 
+
+const keys = Object.keys(student);
+const values = Object.values(student);
+
+// loop of array 
+// no return uses forEach()
+const numbers = [76, 22, 24, 86, 95];
+numbers.forEach(num => console.log(num));
+
+// return uses map
+numbers.map(num => {
+    const mul = num * 5;
+    console.log(mul);
+})
+// for-of on array like object
+//loop on an object using  for-in 
+
+
+
+const products = [
+    { name: 'laptop', price: 3200, brand: 'lenovo', color: 'silver' },
+    { name: 'phone', price: 7000, brand: 'iphone', color: 'golden' },
+    { name: 'watch', price: 3000, brand: 'casio', color: 'yellow' },
+    { name: 'sungalss', price: 300, brand: 'ray', color: 'black' },
+    { name: 'camera', price: 9000, brand: 'canon', color: 'gray' }
+];
+
+const newProduct = { name: 'webcam', price: 2800, brand: "rapoo" };
+
+const newProducts = [...products, newProduct];
+
+/// creat a new array without one specific item or
+// remove phone means create a new array without the phone
+
+const filteringProduct = products.filter(pd => pd.name !== 'phone');
+console.log(filteringProduct);
+
+
