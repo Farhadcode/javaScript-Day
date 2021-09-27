0ae9a8e61025a29408623e1d1d3c9bd5dadcd3f1
Farhadcode <farhad.ahmed.aust@gmail.com>
2021-09-27 19:31:59 +0600

    data add and remove

--- slider/array add and remove in object/app.js
+++ slider/array add and remove in object/app.js
@@ -1,11 +1,33 @@
-const data = {};
+
+// add data in a DataBaise
 
 const addToData = item => {
     const data = getData();
-    data[item] = 1;
+    if (item in data) {
+        data[item] += 1;
+    }
+    else {
+        data[item] = 1;
+    }
     console.log(data);
+    saveToLocalDb(data);
+}
+
+// remove data in a DataBaise
+
+const removeFromData = (item) => {
+    const data = getData();
+    delete data[item];
+    saveToLocalDb(data)
+}
+
+const saveToLocalDb = data => {
+    localStorage.setItem('shopping-cart', JSON.stringify(data));
 }
 
 const getData = () => {
-    return data;
+    // return data;
+    // const data = {};
+    const savedDb = localStorage.getItem('shopping-cart');
+    return JSON.parse(savedDb);
 }
